ee243f75a0e5b07449e1ef52c9e755b8d8c7cce8
gprakashgoud <gprakashgoud611@gmail.com>
2025-09-09 22:07:17 +0530

    Patterns

---
+++ Pattern_1.java
@@ -0,0 +1,21 @@
+package com.codegnan.patternexamples;
+
+public class Pattern_1 {
+
+	public static void main(String[] args) {
+ for(int i=1;i<=5;i++) {
+	 for(int j=1;j<=5;j++) {
+		 //System.out.print("* ");//
+		 //System.out.print(j+" ");//
+		 //System.out.print((6-j)+" ");//
+		 //System.out.print(i+" ");//
+		 //System.out.print((6-i)+" ");//
+		 //System.out.print((char)(96+j)+" ");//
+		 System.out.print((char)(102-j)+" ");
+
+	 }
+	 System.out.println();
+ }
+	}
+
+}
