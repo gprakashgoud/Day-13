ad862cdf3c7624fca4cdd077581e0c7000daf9df
gprakashgoud <gprakashgoud611@gmail.com>
2025-09-09 22:08:49 +0530

    Patterns

---
+++ Pattern_triangle.java
@@ -0,0 +1,22 @@
+package com.codegnan.patternexamples;
+
+public class Pattern_triangle {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("* ");
+				// System.out.print(j+" ");//
+				// System.out.print(6-j+" ");//
+				// System.out.print(i+" ");//
+				// System.out.print((6-i)+" ");//
+				// System.out.print((char)(96+j)+" ");//
+				// System.out.print((char)(102-j)+" ");//
+				// System.out.print((char)(96+i)+" ");//
+				// System.out.print((char)(102-i)+" ");//
+			}
+
+			System.out.println();
+		}
+	}
+}
